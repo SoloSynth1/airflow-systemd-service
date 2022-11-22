038683d4f8fa8824fcec8bcfadcd6eb53c3279fd
SoloSynth1 <solosynth1@gmail.com>
2022-11-21 16:10:09 -0800

    add SCRIPT_DIR to fix error running `run.sh` in other directories

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Get current location of this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Copy service files to systemd
-cp airflow-*.service /lib/systemd/system/
+cp $SCRIPT_DIR/airflow-*.service /lib/systemd/system/
 
 # Enable Airflow services
 systemctl enable airflow-webserver.service
